b5fbffcb9ade34a2d4692bab45d9dc0a14523dc6
XEDONO <50533153+XEDONO@users.noreply.github.com>
2025-11-11 18:48:39 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NEXTCRM/',
+  base: '/NEXTCRM/', // This is the subfolder on GitHub Pages
+  build: {
+    rollupOptions: {
+      input: 'index.html' // Explicitly set the entry point
+    }
+  }
 })
