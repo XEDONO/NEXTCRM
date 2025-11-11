5927331124ce1a683a2e9e09613e010e087ea97c
XEDONO <50533153+XEDONO@users.noreply.github.com>
2025-11-11 20:12:41 +0000

    Revert: Remove HashRouter, restore working state-based routing

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,8 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/NEXTCRM">
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
